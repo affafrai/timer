b668fcb1e4e871e98e254e42084d1350043832d6
affaf <affaf.rai@hotmail.com>
2020-08-25 23:48:52 +0000

    commit timer1

--- timer1.js
+++ timer1.js
@@ -7,9 +7,10 @@ let newArray = args.slice(2);
 for (let i = 0; i < newArray.length; i++){
   
   let time = parseInt(newArray[i]);
-setTimeout(() => {
-
-  process.stdout.write('\x07');
-  
-}, time);
+  if (time > 0){
+    setTimeout(() => {
+      console.log(time)
+    process.stdout.write('\x07');
+    }, time);
+  }
 }
